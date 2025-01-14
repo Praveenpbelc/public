4b7a1e60feee6c0c6e75299d5af48e4a5668fb40
praveen kumar <praveenkumarpb1011@gmail.com>
2025-01-14 20:45:34 +0530

    finalsite

---
+++ intex.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBysYTI1LLHIxyEPhEvEyBUFShy6LqAj4Q",
+  authDomain: "elctech-65a0a.firebaseapp.com",
+  databaseURL: "https://elctech-65a0a-default-rtdb.firebaseio.com",
+  projectId: "elctech-65a0a",
+  storageBucket: "elctech-65a0a.firebasestorage.app",
+  messagingSenderId: "523246460257",
+  appId: "1:523246460257:web:d765b0361c61c048bc7920"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
